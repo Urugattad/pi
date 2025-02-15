cbb1c4a52320208d3b2a257ca3381bfd918d4319
Urugattad <135198026+Urugattad@users.noreply.github.com>
2025-02-15 21:39:19 +0530

    Add files via upload

---
+++ allah.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify, render_template
+import random
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('std.html')  # Load the HTML page
+
+@app.route('/toggle_gpio', methods=['POST'])
+def submit():
+    data = request.get_json()  # Get JSON data from JavaScript
+    name = data.get('name', '')  # Extract 'name' from JSON data
+    pin = int(data.get('pin',0))
+    state = data.get('state', False)
+    status = "ON" if state else "OFF"
+    return jsonify(message=f"Hello, {name} {name}! Your gpio number : {pin} {status}")  # Send JSON response
+
+@app.route('/get_temperature', methods=['GET'])
+def get_temperature():
+    temperature = round(random.uniform(20, 35), 2)  # Simulated data
+    return jsonify (message=f"Temperature : {temperature} °C")
+
+
+@app.route('/get_pir', methods=['GET'])
+def get_pir():
+    motion_detected = random.choice([True, False])  # Simulated PIR data
+    return jsonify(message=f"{motion_detected}" )
+
+if __name__ == '__main__':
+    app.run(debug=True)
